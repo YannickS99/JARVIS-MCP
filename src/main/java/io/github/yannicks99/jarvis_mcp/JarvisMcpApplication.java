2471af35e4fabe4cc72b85daa0474942558bb681
YannickS99 <schroth.yannick@gmail.com>
2026-09-12 15:33:31 +0200

    Initial Commit

---
+++ src/main/java/io/github/yannicks99/jarvis_mcp/JarvisMcpApplication.java
@@ -0,0 +1,13 @@
+package io.github.yannicks99.jarvis_mcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JarvisMcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JarvisMcpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/yannicks99/jarvis_mcp/JarvisMcpApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.yannicks99.jarvis_mcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JarvisMcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
